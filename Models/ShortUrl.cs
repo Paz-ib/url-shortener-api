fbf07fbb6d9d606669f9a8f61ea4b80129cd9d25
Paz <mariapazibarra@MacBook-Air-de-Maria.local>
2026-03-09 15:58:09 -0300

    Add ShortUrl model

---
+++ Models/ShortUrl.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UrlShortenerApi.Models;
+
+public class ShortUrl
+{
+    public int Id { get; set; }
+    public string OriginalUrl { get; set; } = string.Empty;
+    public string ShortCode { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
